fbd6c3a2af88c2ee112455690ff8c30dc377b9be
Sahil <sahil.dixit15.sd@gmail.com>
2022-11-22 13:00:10 +0530

    changed windows location replace

--- public/assets/index.js
+++ public/assets/index.js
@@ -1,4 +1,4 @@
 const btnClick = document.getElementById("btn");
 btnClick.addEventListener("click", function () {
-    window.location.replace(`http://localhost:3000/studentForm`);
+    window.location.replace(`../studentForm`);
 });
