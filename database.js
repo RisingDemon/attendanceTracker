a88b1e9b3a05c790f2553319dec263609ab3a884
Sahil <sahil.dixit15.sd@gmail.com>
2023-01-19 10:29:05 +0530

    added timeout

--- database.js
+++ database.js
@@ -9,6 +9,7 @@ const connection = mysql.createConnection({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
+    connectTimeout: 10000,
     // port: process.env.PORT,
     // insecureAuth: true,
 });
